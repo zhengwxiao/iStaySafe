2d6f230d5e33668b4651aff336c32b873a1b4666
Zheng Xiao <zwxxiao@gmail.com>
2020-03-28 18:58:19 -0500

    Added notifications scheduler

--- iStaySafe/AppDelegate.swift
+++ iStaySafe/AppDelegate.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let notificationController = NotificationController()
+        
+        notificationController.notificationCenter.delegate = self
+        
         return true
     }
 
---
+++ iStaySafe/Notifications/NotificationController.swift
@@ -0,0 +1,46 @@
+//
+//  NotificationController.swift
+//  iStaySafe
+//
+//  Created by Z Xiao on 3/28/20.
+//  Copyright © 2020 Z Xiao. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UserNotifications
+
+class NotificationController: NSObject {
+    static let shared = NotificationController()
+    let notificationCenter = UNUserNotificationCenter.current()
+    
+    // Transferred to ExensionDelegate
+    func scheduleReminders(title: String, body: String, delay: Double) {
+        // notificationCenter.removeAllPendingNotificationRequests()
+        
+        notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            print("Notification authorization: \(granted)")
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = UNNotificationSound.default
+           
+        // Create the trigger as a repeating event.
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: delay, repeats: false)
+        
+        // Create the request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                    content: content, trigger: trigger)
+
+        // Schedule the request with the system.
+        notificationCenter.add(request) { (error) in
+           if error != nil {
+              // Handle any errors.
+           }
+        }
+    }
+}
